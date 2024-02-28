18234bf55c022a19b28b970cdf34f4297e668dab
nizarmahi <mahinizar7@gmail.com>
2024-02-28 21:02:16 +0700

    Praktikum 6

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +22,5 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class,'index']);
+
+Route::get('/user', [UserController::class, 'index']);
