8042044cb4288223d0843b9ca26484ed1d23f4ad
nizarmahi <mahinizar7@gmail.com>
2024-03-06 09:19:46 +0700

    Praktikum 2.2 not found exception

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,9 +22,11 @@ class UserController extends Controller
         // coba akses model UserModel
         // $user = UserModel::where('level_id', 1);
 
-        $user = UserModel::findOr(20,['username','nama'], function(){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20,['username','nama'], function(){
+        //     abort(404);
+        // });
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]); 
     }   
 }
