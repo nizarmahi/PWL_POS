b105e958745e55da3cc907983b290f34a7e891b9
nizarmahi <mahinizar7@gmail.com>
2024-03-06 08:16:02 +0700

    Praktikum Fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,20 +9,15 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1', 
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'), 
-        //     'level_id' => 4,
-        // ];
-
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
-
+        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'username' => 'manager_tiga', 
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'), 
+            'level_id' => 2,
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+
+        UserModel::insert($data); // tambahkan data ke tabel m_user
         
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
