28bff7298e41273980fbf821a04ac3b6848719f7
nizarmahi <mahinizar7@gmail.com>
2024-03-06 10:04:42 +0700

    Praktikum 2.4 – Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,19 +6,17 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                {{-- <th>ID</th>
+                <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
-                <th>ID Level Pengguna</th> --}}
-                <td>Jumlah Pengguna</td>
+                <th>ID Level Pengguna</th>
             </tr>
             {{-- @foreach ($data as $d)  --}}
             <tr>
-                {{-- <td>{{ $data->user_id }}</td> 
+                <td>{{ $data->user_id }}</td> 
                 <td>{{ $data->username }}</td> 
                 <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td> --}}
-                <td>{{ ($data) }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
             {{-- @endforeach --}}
         </table>
