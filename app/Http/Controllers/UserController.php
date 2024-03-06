0250bf753e09cb26a2e2ea75f8b62819ecba47ba
nizarmahi <mahinizar7@gmail.com>
2024-03-06 09:13:02 +0700

    Praktikum 2.1 retrieving single models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,18 +9,22 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_3', 
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'), 
-        ];
+        // // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_3', 
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'), 
+        // ];
 
-        UserModel::insert($data); // tambahkan data ke tabel m_user
+        // UserModel::insert($data); // tambahkan data ke tabel m_user
         
         // coba akses model UserModel
-        $user = UserModel::all();
+        // $user = UserModel::where('level_id', 1);
+
+        $user = UserModel::findOr(20,['username','nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]); 
     }   
 }
